d34db7786b08137645d42d1a59447c54b183d5f1
Rumeysa Başaran <rumeysa.basaran@gmail.com>
2022-09-06 16:38:18 +0300

    commited

--- src/TaksimetreProgrami.java
+++ src/TaksimetreProgrami.java
@@ -1,6 +1,12 @@
 import java.util.Scanner;
 public class TaksimetreProgrami {
     public static void main(String[] args) {
+        //Gereksinimler:
+        //Taksimetre KM başına 2.20 TL tutmaktadır.
+        //Minimum ödenecek tutar 20 TL'dir. 20 TL altında ki ücretlerde yine 20 TL alınacaktır.
+        //Taksimetre açılış ücreti 10 TL'dir.
+
+
         int km;
         double perKm = 2.20;
         double total;
